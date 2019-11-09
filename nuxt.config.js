d5297716f57a7b108920734bbb927e8f741e12f2
KeisukeToyota <hm.pudding0715@gmail.com>
2019-11-10 05:21:23 +0900

    fix layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ module.exports = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/vuetify'
   ],
   router: {
     mode: 'hash'
